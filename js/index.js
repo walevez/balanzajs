122d37947056add9525f7758a9905f951394a5dd
walevez <walevez@gmail.com>
2023-01-22 08:37:59 -0300

    more styles

--- js/index.js
+++ js/index.js
@@ -11,29 +11,38 @@ tara.setAttribute('value', 0);
 excedente.setAttribute('value', 0);
 
 
-// Por cada numuero ingresa devuelve el resultado de la resta
+// Por cada numero ingresado devuelve el resultado de la resta
 const operacion = ()=>{
     const calculo = parseInt(bruto.value) - parseInt(tara.value) - parseInt(excedente.value);
     neto.setAttribute('value', calculo);
     if (neto.value < 0 || neto.value > 60000){
-        neto.style.background = "yellow";
+        neto.style.background = "yellow"
+        neto.style.color = "red"
     } else {
-        neto.style.background = "azure"
+        neto.style.background = "green"
+        neto.style.color = "white"
     }
 };
+
+// Pone un cero si el campo queda NaN
+const allwaysInt = (field)=>{
+    if (field.value == ""){
+        field.value = 0;
+        operacion()
+    };
+};
+
+// Da Focus y selecciona el contenido de un input
+const selectValue = (field)=>{
+    field.focus();
+    field.select();
+};
+
 
 // Llama a la funcion cada vez que se presiona una tecla dentro del input
 bruto.addEventListener('keyup', operacion);
 tara.addEventListener('keyup', operacion);
 excedente.addEventListener('keyup', operacion);
-
-
-// Esta funcion pone un cero si el campo queda NaN
-const allwaysInt = (field)=>{
-    if (field.value == ""){
-        field.value = 0;
-    };
-};
 
 // Llama a la funcion cada vez que el input pierde el focus
 bruto.addEventListener('blur', ()=>{
@@ -45,14 +54,6 @@ tara.addEventListener('blur', ()=>{
 excedente.addEventListener('blur', ()=>{
     allwaysInt(excedente);
 });
-
-
-// Da Focus y selecciona el contenido de un input
-const selectValue = (field)=>{
-    field.focus();
-    field.select();
-};
-
 
 // Llama a la funcion cada vez que se hace click en el input
 bruto.addEventListener('click', ()=>{
